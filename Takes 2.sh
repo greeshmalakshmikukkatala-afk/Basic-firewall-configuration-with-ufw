0937ab81167ef128796a0399963a2cc5feda7fdd
greeshmalakshmikukkatala-afk <greeshmalakshmikukkatala@gmail.com>
2026-04-15 16:54:06 +0530

    Add files via upload

---
+++ Takes 2.sh
@@ -0,0 +1,13 @@
+Task2:basic firewall configuration with ufw
+
+1.sudo apt update
+
+2.sudo apt install -y ufw
+
+3.sudo ufw allow ssh   
+
+4. sudo ufw deny http
+
+5.sudo ufw enable 
+
+6. sudo ufw status verbose
